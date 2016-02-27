c5f044bde220c3b9ff46c3a5a39bdc0fe9b86472
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2016-02-27 18:16:24 -0500

    Routes set up for site

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var index = require('./routes/index');
+var dinos = require('./routes/dinos');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(require('node-sass-middleware')({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/api/dinos', dinos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- models/dino.js
+++ models/dino.js
@@ -4,6 +4,8 @@ var Schema = mongoose.Schema;
 var Dino = new Schema({
 	content: {type: String, required: true},
 	species: {type: String, required: true, unique: true},
+	upvotes: {type: Number, default: 0, required: true},
+	downvotes: {type: Number, default: 0, required: true},
 	img: {type: String}
 }, {collection: 'Dino'});
 
