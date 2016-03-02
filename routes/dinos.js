0fab1b94e1c3fb0580c7f4323f174ecd59dd1eb1
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2016-03-02 17:24:18 -0500

    Removed logs in dinos.js

--- routes/dinos.js
+++ routes/dinos.js
@@ -9,7 +9,6 @@ router.get('/', function(req, res, next) {
   	if (!err) {
   		res.send(dinos);
   	} else {
-  		console.log(err),
   		res.send(err);
   	}
   })
@@ -19,24 +18,20 @@ router.post('/', function(req, res, next) {
 	var dino = req.body;
 	(new Dino(req.body)).save(function(err, dino) {
 		if (!err) {
-			console.log(dino);
 			res.send(dino);
 		} else {
-			console.log(err);
 			res.send(err);
 		}
 	})
 });
 
 router.put('/', function(req, res, next) {
-	console.log(req.body);
 	var body = req.body;
 	var dinoId = new ObjectId(body._id);
 
 	Dino.findOne({
 		'_id': dinoId
 	}, function(err, dino) {
-		console.log(dino);
 		if (!err) {
 			dino.species = body.species;
 			dino.content = body.content;
@@ -44,14 +39,12 @@ router.put('/', function(req, res, next) {
 			dino.downvotes = body.downvotes;
 			dino.save(function(err) {
 				if (err) {
-					console.log(err);
 					res.send(err);
 				} else {
 					res.send(dino);
 				}
 			});
 		} else {
-			console.log(err);
 			res.send(err);
 		}
 	})
@@ -65,7 +58,6 @@ router.delete('/', function(req, res, next) {
 		if (!err) {
 			res.send(dino);
 		} else {
-			console.log(err),
 			res.send(err);
 		}
 	})
