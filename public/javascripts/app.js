374841562138f3bf344915bf6a00fb0b2dac96c1
Filippos Lymperopoulos <filippos.lymperopoulos@students.olin.edu>
2016-04-10 21:55:43 -0400

    ADD: feedback lab1

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// Love grunt and nice breakdown of frontend js scripts - some commenting would be nice though people!!
 var DinoList = require('./dinolist.jsx');
 var DinoDetail = require('./dinodetail.jsx');
 var DinoForm = require('./dinoForm.jsx');
@@ -153,6 +154,7 @@ var DinoApp = React.createClass({displayName: "DinoApp",
     return (
       React.createElement("div", null, 
         React.createElement("audio", {id: "audio", controls: true, autoPlay: true}, 
+        	// YES
           React.createElement("source", {src: "http://soundbible.com/grab.php?id=1782&type=mp3", type: "audio/mpeg"})
         ), 
         React.createElement(DinoList, {
